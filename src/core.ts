90dfe6c2396ea17d8cd1c56ee85afb7a5d1f7c9b
falsepopsky <falsepopsky@protonmail.com>
2023-04-03 01:14:14 -0300

    refactor: rewrite core class of TheTVDB

---
+++ src/core.ts
@@ -0,0 +1,39 @@
+import { URL } from 'node:url';
+import type { FetchResult } from './types.js';
+
+export class Base {
+  private readonly _token;
+  protected readonly api = 'https://api4.thetvdb.com';
+
+  constructor(token: string) {
+    this.validateInput(token, 'Token is required');
+    this._token = token;
+  }
+
+  protected async fetcher<T>(url: string | URL): Promise<FetchResult<T>> {
+    const response = await fetch(url, {
+      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${this._token}` },
+    });
+
+    const { status } = response;
+    const result = (await response.json()) as T;
+
+    return { status, result };
+  }
+
+  protected validateInput<T>(input: T, errorMessage: string): void {
+    if (typeof input !== 'string' || input.length === 0) {
+      throw new Error(errorMessage);
+    }
+  }
+
+  protected createQuery<T extends Record<string, string>>(endpoint: string, obj: T): string {
+    const search = new URL(endpoint, this.api);
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === 'string' && value.length >= 1) {
+        search.searchParams.set(key, value);
+      }
+    }
+    return search.href;
+  }
+}
