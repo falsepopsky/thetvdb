e7d90b08334780855b50cd243658b70f0f7624d1
falsepopsky <falsepopsky@protonmail.com>
2023-04-11 17:39:27 -0300

    fix: load the .env file from any dir.

--- src/playground.ts
+++ src/playground.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
+import { join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { TheTVDBExtended } from './index.js';
 
-dotenv.config({ path: '../.env' });
+const ROOT_DIR = resolve(fileURLToPath(import.meta.url), '../..');
+const ENVFILE = join(ROOT_DIR, '.env');
+
+dotenv.config({ path: ENVFILE });
 
 const TOKEN = process.env.TVDB_API_TOKEN;
 const clientExtended = new TheTVDBExtended(TOKEN);
 
 const { result } = await clientExtended.getLanguages();
 
-console.table(result);
+console.log(result);
