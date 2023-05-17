2ddaf72265a90ebf23e001014fb2087de15b4ceb
falsepopsky <falsepopsky@protonmail.com>
2023-05-16 23:57:18 -0300

    feat: add AbortController

--- src/core.ts
+++ src/core.ts
@@ -1,25 +1,58 @@
+import { clearTimeout, setTimeout } from 'node:timers';
 import { URL } from 'node:url';
 
 export class Base {
   private readonly _token;
   protected readonly api = 'https://api4.thetvdb.com';
+  private _timeout: number;
 
-  constructor(token: string) {
+  constructor(token: string, timeout = 5000) {
     this.validateInput(token, 'Token is required');
+    this.validateTimeout(timeout);
     this._token = token;
+    this._timeout = timeout;
+  }
+
+  public getTime(): number {
+    return this._timeout;
+  }
+
+  public setTime(value: number): void {
+    this.validateTimeout(value);
+    this._timeout = value;
   }
 
   protected async fetcher<T>(url: string | URL): Promise<T> {
-    const response = await fetch(url, {
-      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${this._token}` },
-    });
+    const controller = new AbortController();
+    const { signal } = controller;
+    const timeout = setTimeout(() => {
+      controller.abort();
+    }, this._timeout);
 
-    return (await response.json()) as T;
+    try {
+      const response = await fetch(url, {
+        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${this._token}` },
+        signal,
+      });
+
+      return (await response.json()) as T;
+    } finally {
+      clearTimeout(timeout);
+    }
   }
 
   protected validateInput(input: string, message: string): void {
     if (typeof input !== 'string' || input.length === 0) {
       throw new Error(message);
+    }
+  }
+
+  protected validateTimeout(input: number): void {
+    if (typeof input !== 'number' || isNaN(input)) {
+      throw new TypeError('timeout must be of type number');
+    }
+    if (input <= 0) {
+      throw new RangeError('timeout must be a positive number');
     }
   }
 
