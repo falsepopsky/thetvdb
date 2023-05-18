ee842347fab8548a4698b3ee59008c5248bd4900
falsepopsky <falsepopsky@protonmail.com>
2023-05-17 22:54:35 -0300

    chore(project): update pnpm and enviroment

--- src/enviroment.d.ts
+++ src/enviroment.d.ts
@@ -1,9 +1,12 @@
+import type Undici from 'undici';
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       TVDB_API_TOKEN: string;
     }
   }
+  export const fetch: typeof Undici.fetch;
 }
 
 export {};
