866a665c6c06edb3424a59e250958df913511df4
falsepopsky <falsepopsky@protonmail.com>
2023-12-02 22:11:26 -0300

    chore: move playground to rootDir '.'

---
+++ playground.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+import { TheTVDB } from './packages/thetvdb/src/index.js';
+
+config(); // Load .env file
+
+const client = new TheTVDB(process.env.TVDB_API_TOKEN);
+const movie = await client.getCompanies('94');
+console.log(movie);
+
+/*
+const clientExtended = new TheTVDBExtended(process.env.TVDB_API_TOKEN);
+const langs = await clientExtended.getLanguages();
+console.log(langs);
+*/
