d2e6a923f839d2b301220a512e24a1e460bbd797
falsepopsky <falsepopsky@protonmail.com>
2023-12-13 03:51:59 -0300

    fix: remove unused extension

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,7 +2,7 @@ import starlightPlugin from '@astrojs/starlight-tailwind';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,mdx}'],
   theme: {
     extend: {},
   },
