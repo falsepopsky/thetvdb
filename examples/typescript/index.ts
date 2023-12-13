7f95937cd433c96ee86b63289c5d1ddc7c58e3d7
falsepopsky <falsepopsky@protonmail.com>
2023-12-13 19:17:30 -0300

    chore: update examples

--- examples/typescript/index.ts
+++ examples/typescript/index.ts
@@ -1,6 +1,5 @@
-import { TheTVDBExtended } from '@untidy/thetvdb';
+import { TheTVDB } from '@untidy/thetvdb';
 
-const token = 'my secret token';
-const client = new TheTVDBExtended(token);
-const { data } = await client.getCountries();
+const client = new TheTVDB('Your secret token');
+const { data } = await client.countries();
 console.log(data);
