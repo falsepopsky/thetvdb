ae34c32234203db5600038acf54d13992f39f53e
falsepopsky <falsepopsky@protonmail.com>
2023-12-18 14:11:36 -0300

    chore: update playground

--- playground.ts
+++ playground.ts
@@ -1,14 +1,9 @@
 import { config } from 'dotenv';
 import { TheTVDB } from './packages/api/src/index.js';
 
-config(); // Load .env file
+// Load .env file
+config();
 
 const client = new TheTVDB(process.env.TVDB_API_TOKEN);
-const movie = await client.getCompanies('94');
-console.log(movie);
-
-/*
-const clientExtended = new TheTVDBExtended(process.env.TVDB_API_TOKEN);
-const langs = await clientExtended.getLanguages();
-console.log(langs);
-*/
+const { data } = await client.serieEpisodes({ id: '75978', seasonType: 'default' });
+console.log(data);
