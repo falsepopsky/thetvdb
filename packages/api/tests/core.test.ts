8dacad46e8dad57e6f0b23efa84d9a9c91476b29
falsepopsky <falsepopsky@protonmail.com>
2024-01-11 17:31:08 -0300

    test: add tests for TheTVDB constructor

--- packages/api/tests/core.test.ts
+++ packages/api/tests/core.test.ts
@@ -1,23 +1,21 @@
-import { Base } from '../src/core.js';
+import { TheTVDB } from '../src/index.js';
 
-describe('constructor token validation tests', () => {
+describe('TheTVDB constructor token validation tests', () => {
   it('throws an error if no token is provided', () => {
-    // @ts-expect-error: Cannot create an instance of an abstract class.
-    expect(() => new Base()).toThrow('Token is required');
+    // @ts-expect-error: Expected token argument.
+    expect(() => new TheTVDB()).toThrow('Token is required');
   });
 
   it('throws an error if an empty string is provided', () => {
-    // @ts-expect-error: Cannot create an instance of an abstract class.
-    expect(() => new Base('')).toThrow('Token is required');
+    expect(() => new TheTVDB('')).toThrow('Token is required');
   });
 
   it('throws error when non-string token is provided', () => {
-    // @ts-expect-error: Cannot create an instance of an abstract class.
-    expect(() => new Base(1234)).toThrow('Token is required');
+    // @ts-expect-error: Argument of type 'number' is not assignable to parameter of type 'string'.
+    expect(() => new TheTVDB(1234)).toThrow('Token is required');
   });
 
   it('creates an instance when a valid token is provided', () => {
-    // @ts-expect-error: Cannot create an instance of an abstract class.
-    expect(() => new Base('fake token')).not.toThrow();
+    expect(() => new TheTVDB('fake token')).not.toThrow();
   });
 });
