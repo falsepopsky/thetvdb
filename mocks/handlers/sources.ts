506dbb8df5d68cf0294a92e38ca78d000eed7f8b
falsepopsky <falsepopsky@protonmail.com>
2024-01-12 05:30:23 -0300

    test: add `/sources/types`

---
+++ mocks/handlers/sources.ts
@@ -0,0 +1,28 @@
+import { HttpResponse } from 'msw';
+
+// https://api4.thetvdb.com/v4/sources/types
+const sourcesTypes = {
+  status: 'success',
+  data: [
+    {
+      id: 2,
+      name: 'IMDB',
+      slug: 'imdb',
+      prefix: 'https://www.imdb.com/title/',
+      postfix: '/',
+      sort: 4,
+    },
+    {
+      id: 3,
+      name: 'TMS (Zap2It)',
+      slug: 'zap2it',
+      prefix: 'https://tvlistings.zap2it.com/overview.html?programSeriesId=',
+      postfix: null,
+      sort: 6,
+    },
+  ],
+};
+
+export function handleSourcesTypes(): HttpResponse {
+  return HttpResponse.json(sourcesTypes);
+}
