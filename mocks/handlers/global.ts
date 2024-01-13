60f0a4fdec9bcfaeeecabdc076a982dbce010aff
falsepopsky <falsepopsky@protonmail.com>
2024-01-13 02:18:35 -0300

    test: add `/content/*`

--- mocks/handlers/global.ts
+++ mocks/handlers/global.ts
@@ -2,5 +2,6 @@ export { artworkPaths } from './artwork.js';
 export { awardsPaths } from './awards.js';
 export { charactersPaths } from './characters.js';
 export { companiesPaths } from './companies.js';
+export { contentPaths } from './content.js';
 export { sourcesTypesPaths } from './sources.js';
 export { updatesPaths } from './updates.js';
